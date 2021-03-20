1329fab94426b97d099d42b4da9821117786e6e8
David Asensio Cañas <david.asensio@icloud.com>
2021-03-20 19:13:35 +0100

    Fix gpg issues

---
+++ doc/troubleshooting.md
@@ -0,0 +1,7 @@
+# Troubleshooting
+
+This is a trobleshooting guide to help my future self solve problems with my dotfiles
+
+## Issues with GPG and git
+
+To solve this kind of issues, follow the steps described in [this gist](https://gist.github.com/troyfontaine/18c9146295168ee9ca2b30c00bd1b41e)
--- shell/exports.sh
+++ shell/exports.sh
@@ -2,6 +2,8 @@ export JAVA_HOME='/Library/Java/JavaVirtualMachines/amazon-corretto-15.jdk/Conte
 export GEM_HOME="$HOME/.gem"
 export GOPATH="$HOME/.go"
 
+export GPG_TTY=`tty`
+
 export FZF_DEFAULT_OPTS='
   --color=pointer:#ebdbb2,bg+:#3c3836,fg:#ebdbb2,fg+:#fbf1c7,hl:#8ec07c,info:#928374,header:#fb4934
   --reverse
