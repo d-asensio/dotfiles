af160353d84cdf625f07c19175133ff1387a631d
David Asensio Cañas <david.asensio@icloud.com>
2021-03-20 19:13:40 +0100

    Attempt to fix installation script

--- install.sh
+++ install.sh
@@ -2,17 +2,21 @@
 
 # Temporal variables for the installation to work, once dotfiles are installed, those are exported and available
 # everywhere.
-export DOTFILES_PATH="$HOME/.dotfiles"
-export DOTLY_PATH="$DOTFILES_PATH/modules/dotly"
-export DOTLY_ENV=PROD
+DOTFILES_PATH="$HOME/.dotfiles"
+DOTLY_PATH="$DOTFILES_PATH/modules/dotly"
+
+# Export variables that are needed by child scripts
+export DOTFILES_PATH="$DOTFILES_PATH"
+export DOTLY_PATH="$DOTLY_PATH"
+export DOTLY_ENV="PROD"
 
 # Installation
 
 # Clone dotfiles repository
-git clone https://github.com/d-asensio/dotfiles $HOME/.dotfiles
+git clone https://github.com/d-asensio/dotfiles "$HOME/.dotfiles"
 
 # Go to dotfiles folder
-cd $HOME/.dotfiles
+cd "$DOTFILES_PATH"
 
 # Install git submodules
 git submodule update --init --recursive
