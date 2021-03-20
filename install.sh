e6bbc23d71b7b3e19533e4394a85f06a5b4fa995
David Asensio Cañas <david.asensio@icloud.com>
2021-03-20 19:13:36 +0100

    Add clone step to install script

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+git clone https://github.com/d-asensio/dotfiles $HOME/.dotfiles
 cd $HOME/.dotfiles
 git submodule update --init --recursive
 DOTFILES_PATH="$HOME/.dotfiles" DOTLY_PATH="$DOTFILES_PATH/modules/dotly" "$DOTLY_PATH/bin/dot" self install
