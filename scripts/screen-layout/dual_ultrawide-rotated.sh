0d2c962e8c9e0addb57d7c63b40da542be7cf6ff
David Asensio Cañas <david.asensio@icloud.com>
2022-01-08 16:49:38 +0100

    Add script to set up screen layout

---
+++ scripts/screen-layout/dual_ultrawide-rotated.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output eDP-1 --off --output DP-1 --off --output DP-2 --primary --mode 3440x1440 --pos 0x611 --rotate normal --output DP-1-8 --mode 2560x1440 --pos 3440x0 --rotate right --output DP-1-1 --off
